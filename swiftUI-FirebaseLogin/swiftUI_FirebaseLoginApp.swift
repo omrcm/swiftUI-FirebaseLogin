60ea01bc93e52cc9bfd7522c9c02ffca1cee4d0b
Ömür Uçum <omurucum@gmail.com>
2023-09-17 14:19:52 +0300

    Initial Commit

---
+++ swiftUI-FirebaseLogin/swiftUI_FirebaseLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_FirebaseLoginApp.swift
+//  swiftUI-FirebaseLogin
+//
+//  Created by Ömür Uçum on 17.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_FirebaseLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
